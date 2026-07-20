157300e8a65a2ac7b0ddcd660fe3d943dd23ee1e
tthiennes-eng <tthiennes@gmail.com>
2026-07-20 04:52:26 -0300

    9

--- backend/DentalClinic.Api/Program.cs
+++ backend/DentalClinic.Api/Program.cs
@@ -20,7 +20,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 2. Injeção de Dependências (DI) - Repositories
+// 2. Injeção de Dependência (DI) - Repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
